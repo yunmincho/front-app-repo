247d1f369113fa02d9a6ee9e896dfb8feb52a138
yunmincho <cym9325@naver.com>
2024-06-10 01:05:01 +0000

    Title Update

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version 1
+            ISfnet HomePage
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
